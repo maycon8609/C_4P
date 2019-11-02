bc06e519d2d845f9278c9654428429fcd06cd4af
maycon8609 <maycon8609@gmail.com>
2019-11-02 18:06:05 -0300

    cadastro de funcionarios pronto

--- project/ProjectVendaTst01/main.c
+++ project/ProjectVendaTst01/main.c
@@ -4,28 +4,34 @@
 #include<stdbool.h>
 
 typedef struct produtos{
-  char nome[40];
+  char nome[100];
   float precoCompra;
   float precoVenda;
   float lucro;
-  char tipo[20];
-  char codProd[3];
-
+  char tipo[50];
+  char codProd[50];
 }produtos;
 
-// VETORES
-produtos vetProd[100];
+typedef struct funcionario{
+  char nome[100];
+  char codFunc[50];
+}funcionario;
+
+/* VETORES */
+produtos vetProd[1000];
 int tprod = 0;
 
+funcionario vetFunc[100];
+int tfunc = 0;
 
-// PONTEIROS
+/* PONTEIROS */
 FILE *fprod;
+FILE *ffunc;
 
-
-// FUNCOES GLOBAIS
+/* FUNCOES GLOBAIS */
 
 void abrirArquivos(){
-  // Arquivo de produtos
+  /* Arquivo de produtos */
   if(fprod = fopen("produtos.txt", "r")){
     while((fscanf(fprod, "%s %f %f %f %s %s",
             &vetProd[tprod].nome,
@@ -36,17 +42,22 @@ void abrirArquivos(){
             &vetProd[tprod].codProd)) != EOF){
       tprod++;
     }
-    printf(" produtos.txt ABERTO com exito \n");
-  }else {
-    printf(" erro ao ABRIR o aquivo produtos.txt \n");
+  }
+
+  /* Arquivo de FUNCIONARIOS */
+  if(ffunc = fopen("funcionarios.txt", "r")){
+    while((fscanf(ffunc, "%s %s",
+            &vetFunc[tfunc].nome,
+            &vetFunc[tfunc].codFunc)) != EOF){
+      tfunc++;
+    }
   }
 }
 
 void salvarArquivos(){
-  // Arquivo de produtos
+  /* Arquivo de produtos */
   if(fprod = fopen("produtos.txt", "w")){
     for(int i = 0; i < tprod; i++){
-      fprintf(fprod,"\n");
       fprintf(fprod, "%s\n", vetProd[i].nome);
       fprintf(fprod, "%.2f\n", vetProd[i].precoCompra);
       fprintf(fprod, "%.2f\n", vetProd[i].precoVenda);
@@ -58,6 +69,18 @@ void salvarArquivos(){
     printf(" produtos.txt salvo com sucesso \n");
   }else{
     printf(" erro ao SALVAR produtos.txt \n");
+  }
+
+  /* Arquivo de funcionarios */
+  if(ffunc = fopen("funcionarios.txt", "w")){
+    for(int i = 0; i < tfunc; i++){
+      fprintf(ffunc, "%s\n", vetFunc[i].nome);
+      fprintf(ffunc, "%s\n", vetFunc[i].codFunc);
+    }
+    fclose(ffunc);
+    printf(" funcionarios.txt salvo com sucesso \n");
+  }else{
+    printf(" erro ao SALVAR funcionarios.txt \n");
   }
 }
 
@@ -84,10 +107,13 @@ float lucro(){
 }
 
 void cadastrar(){
+  printf("\33[H\33[2J");
 
   bool varif = false;
   char nome[40];
 
+
+  printf(" CADASTRAR NOVO PRODUTO ");
   printf(" NOME DO PRODUTO ");
   scanf("%s", &nome);
 
@@ -115,6 +141,8 @@ void cadastrar(){
 }
 
 void buscar(){
+  printf("\33[H\33[2J");
+
   char nome[30];
   int verif = 0;
 
@@ -139,6 +167,8 @@ void buscar(){
 }
 
 void editar(){
+  printf("\33[H\33[2J");
+
   char nome[30];
   int verif = 0;
 
@@ -179,6 +209,8 @@ void editar(){
 
 
 void excluir(){
+  printf("\33[H\33[2J");
+
   char nome[30];
   int verif = 0;
 
@@ -208,6 +240,8 @@ void excluir(){
 
 
 void listarGeral(){
+  printf("\e[H\e[2J");
+
   int cont = 1;
   for(int i = 0; i < tprod; i++){
     printf(" PRODUTO %d\n", cont);
@@ -217,12 +251,14 @@ void listarGeral(){
     printf("   LUCRO ------- %.2f\n", vetProd[i].lucro);
     printf("   TIPO PRODUTO  %s\n", vetProd[i].tipo);
     printf("   CODIGO ------ %s\n", vetProd[i].codProd);
-    printf("");
+    printf("\n");
     cont++;
   }
 }
 
 void listTipo(){
+  printf("\33[H\33[2J");
+
   char tipo[20];
   int cont = 1;
   printf(" DIGITE O TIPO A SER LISTADO ");
@@ -237,34 +273,32 @@ void listTipo(){
       printf("   LUCRO ------- %.2f\n", vetProd[i].lucro);
       printf("   TIPO PRODUTO  %s\n", vetProd[i].tipo);
       printf("   CODIGO ------ %s\n", vetProd[i].codProd);
-      printf("");
+      printf("\n");
       cont++;
     }
   }
 }
 
+
 void listar(){
+  printf("\33[H\33[2J");
+
   int op = 0;
 
   do{
-    printf(" 1- GERAL | 2- LISTAR/TIPO | 3- LISTAR/LUCRO | 4- MAIOR VALOR | 5- MENOR VALOR | 0- SAIR ");
+    printf(" 1- GERAL | 2- LISTAR/TIPO | 0- SAIR ");
     scanf("%d", &op);
 
     if(op == 1){
       listarGeral();
     }else if(op == 2){
       listTipo();
-    }else if(op == 3){
-
-    }else if(op == 4){
-
-    }else if(op == 5){
-
     }else if(op != 0){
       printf(" OPCAO INVALIDA ");
     }
 
   }while(op != 0);
+  printf("\33[H\33[2J");
 }
 
 void produto(){
@@ -293,9 +327,173 @@ void produto(){
       printf(" OPCAO INVALIDA \n");
     }
   }while(op != 0);
+  printf("\33[H\33[2J");
 }
-/* FIM FUNCOES DE PRODUTO */
+/* FIM FUNCOES PRODUTO */
 
+/* FUNCOES REFERENTES A FUNCIONARIOS */
+
+bool existeFunc(char funcionario[]){
+  int verif = 0;
+  for(int i = 0; i < tfunc; i++){
+    if(strcmp(funcionario, vetFunc[i].codFunc) == 0){
+      return true;
+      verif = 1;
+      break;
+    }
+  }
+
+  if(verif == 0){
+    return false;
+  }
+}
+
+void cadastrarFunc(){
+  printf("\33[H\33[2J");
+
+  bool varif = false;
+  char codigo[40];
+
+  printf(" CADASTRAR NOVO FUNCIONARIO \n");
+
+  printf(" NOME DO FUNCIONARIO ");
+  scanf("%s", &vetFunc[tfunc].nome);
+
+  printf(" CODIGO DO FUNCIONARIO ");
+  scanf("%s", &codigo);
+  if(!existeFunc(codigo)){
+    sprintf(vetFunc[tfunc].codFunc, "%s", codigo);
+
+    tfunc++;
+  }else {
+    printf(" FUNCIONARIO JA CADASTRADO \n");
+  }
+}
+
+void buscarFunc(){
+  printf("\33[H\33[2J");
+
+  char nome[30];
+  int verif = 0;
+
+  printf(" BUSCAR FUNCIONARIO  ");
+  scanf("%s", &nome);
+
+  for(int i = 0; i < tfunc; i++){
+    if(strcmp(nome, vetFunc[i].nome) == 0){
+      printf(" NOME -- %s\n", vetFunc[i].nome);
+      printf(" CODIGO  %s\n", vetFunc[i].codFunc);
+      verif = 1;
+      break;
+    }
+  }
+  if(verif == 0){
+     printf(" Funcionario nao encontrado ");
+  }
+}
+
+void editarFunc(){
+  printf("\33[H\33[2J");
+
+  char nome[30];
+  int verif = 0;
+
+  printf(" NOME DO FUNCIONARIO A SER EDITADO ");
+  scanf("%s", &nome);
+
+  for(int i = 0; i < tfunc; i++){
+    if(strcmp(nome, vetFunc[i].nome) == 0){
+
+      printf(" INFORME NOVOS DADOS \n");
+
+      printf(" NOME ");
+      scanf("%s", &vetFunc[i].nome);
+
+      printf(" CODIGO DO FUNCIONARIO ");
+      scanf("%s", &vetFunc[i].codFunc);
+
+      verif = 1;
+      break;
+    }
+  }
+
+  if(verif == 0){
+    printf(" Funcionario nao encontrado \n");
+  }
+}
+
+
+void excluirFunc(){
+  printf("\33[H\33[2J");
+
+  char nome[30];
+  int verif = 0;
+
+  printf(" FUNCIONARIO A SER EXCLUIDO ");
+  scanf("%s", &nome);
+
+  for(int i = 0; i < tprod; i++){
+    if(strcmp(nome, vetFunc[i].nome) == 0){
+      strcpy(vetFunc[i].nome, vetFunc[tfunc - 1].nome);
+      strcpy(vetFunc[i].codFunc, vetFunc[tfunc - 1].codFunc);
+      tfunc--;
+      verif = 1;
+      break;
+    }
+  }
+
+  if(verif == 0){
+    printf(" FUNCIONARIO NAO ENCONTRADO \n");
+  }else{
+    printf(" FUNCIONARIO EXCLUIDO COM SUCESSO \n");
+  }
+}
+
+
+void listarFunc(){
+  printf("\e[H\e[2J");
+
+  int cont = 1;
+  for(int i = 0; i < tfunc; i++){
+    printf(" FUNCIONARIO %d\n", cont);
+    printf("   NOME -------  %s\n", vetFunc[i].nome);
+    printf("   CODIGO ------ %s\n", vetFunc[i].codFunc);
+    printf("\n");
+    cont++;
+  }
+
+  if(tfunc == 0){
+    printf(" NAO A FUNCIONARIOS REGISTRADOS \n");
+  }
+}
+
+void funcionarios(){
+  printf("\33[H\33[2J");
+
+  int op = 0;
+
+  do{
+    printf(" 1- CADASTRAR | 2- BUSCAR | 3- EDITAR | 4- EXCLUIR | 5- LISTAR | 0- SAIR ");
+    scanf("%d", &op);
+
+    if(op == 1){
+      cadastrarFunc();
+    }else if(op == 2){
+      buscarFunc();
+    }else if(op == 3){
+      editarFunc();
+    }else if(op == 4){
+      excluirFunc();
+    }else if(op == 5){
+      listarFunc();
+    }else if(op != 0){
+      printf(" OPCAO INVALIDA \n");
+    }
+  }while(op != 0);
+  printf("\33[H\33[2J");
+}
+
+/* FIM FUNCOES FUNCIONARIO */
 
 /* CHAMAR TODAS AS FUNCOES/ MENU PRINCIPAL */
 void exec(){
@@ -311,11 +509,12 @@ void exec(){
     }else if(op == 2){
       produto();
     }else if(op == 3){
-
+      funcionarios();
     }else if(op != 0){
       printf(" OPCAO INVALIDA ");
     }
   }while(op != 0);
+  printf("\33[H\33[2J");
 
   salvarArquivos();
 }
